dfbd4de8722e8a9a2a3b91ae3045c187266e2563
Aida Yrysbekova <aidiko20@gmail.com>
2019-07-29 20:57:22 -0400

    added files and initial database in mysql

---
+++ bamazon.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price DECIMAL(10,2) NULL,
+  quantity INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("MacBook", "Computers", 1000, 50), ("T-Shirt", "Clothing", 20, 100), ("Dining Chair", "Furniture", 300, 5), 
+("Banana", "Produce", 0.45, 500), ("Paper Towels", "Cleaning Supplies", 12.95, 200), ("A4 paper", "Back to school", 3.45, 300),
+("Sandals", "Shoes", 45, 20), ("Wall Art", "Decor", 15, 10), ("Bed Sheets", "Textile", 50, 10), ("Cofee cup", "Household", 5, 20)
