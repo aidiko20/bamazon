978938973ca9cb8c5916c3c79116638f49e5d9c0
Aida Yrysbekova <aidiko20@gmail.com>
2019-08-01 00:38:46 -0400

    added readme file

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# bamazon
+# Bamazon
+Contributor: Aida Yrysbekova
+Created on: 07/29/2019
+Technologies used: Github, MySQL, Node.js, Javascript, Node Packages (MySQL, Inquirer, Console.table)
+-----------------------------
+#Video here ()
+-----------------------------
+#How to use the app:
+1. Open terminal
+2. Navigate to file named bamazonCustomers.js
+3. Install all packages (MySQL, Inquirer, console.table)
+4. In terminal type "node bamazonCustomers.js
+5. Content with inventory will display
+6. You will be asked to input ID number of product you want to buy
+  1. If you enter ID number different from the inventory, you will be asked to input ID number again. 
+7. You will also be asked to input quantity of product you want to buy
+  1. If you input more then actual quantity of product message "Sorry we don't have enough stock" will be displayed
+     1. You will be asked to put ID number of product again
+8. Congratulations you made your purchase.
+9. Your total cost will be displayed
+10. MySQL will update on quantity of the product that was purchased
+11. You will be asked if you want to buy more products
+  1. Choose 'Yes' to buy more
+    1. You will be asked to input ID number again.
+  2. Choose 'No' to stop.
+
+
+#Instruction for building the app
+1. Create a MySQL Database called `bamazon`.
+
+2. Then create a Table inside of that database called `products`.
+
+3. The products table should have each of the following columns:
+
+   * item_id (unique id for each product)
+
+   * product_name (Name of product)
+
+   * department_name
+
+   * price (cost to customer)
+
+   * stock_quantity (how much of the product is available in stores)
+
+4. Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
+
+5. Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+6. The app should then prompt users with two messages.
+
+   * The first should ask them the ID of the product they would like to buy.
+   * The second message should ask how many units of the product they would like to buy.
+
+7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+   * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+
+8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+   * This means updating the SQL database to reflect the remaining quantity.
+   * Once the update goes through, show the customer the total cost of their purchase.
+
--- bamazonCustomers.js
+++ bamazonCustomers.js
@@ -34,25 +34,25 @@ function buyPrompt() {
   inquirer.prompt([
     {
       type: "input",
-      message: "Please type Item ID you like to buy",
+      message: "Please type Item ID you like to buy\r\n",
       name: "ID",
       validate: function (value) {
         if (value!== "" && isNaN(value) == false && value < 11) {
           return true;
         } else {
-          return ("Sorry no item under that id");
+          return ("\r\nSorry no item under that id\r\n");
         }
       }
     },
     {
       type: "input",
-      message: "Please type how many items you would like to buy",
+      message: "Please type how many items you would like to buy\r\n",
       name: "Quantity",
       validate: function (value) {
         if (value!== "" && isNaN(value) == false) {
           return true;
         } else {
-          return ("Please enter a number");
+          return ("\r\nPlease enter a number\r\n");
         }
       }
     }
@@ -60,18 +60,18 @@ function buyPrompt() {
     var query = "SELECT ID, PRODUCT_NAME, DEPARTMENT_NAME, PRICE, QUANTITY FROM products WHERE id = ?";
     connection.query(query, [inputs.ID], function (err, res){
       for (var i = 0; i<res.length; i++) {
-        console.log("Your choice is: " + (res[i].PRODUCT_NAME) + " " + ("in the quantity of ") + (inputs.Quantity));
+        console.log("\r\nYour choice is: " + (res[i].PRODUCT_NAME) + " " + ("in the quantity of ") + (inputs.Quantity));
         if (res[i].QUANTITY < inputs.Quantity) {
-          console.log("Sorry we don't have enough stock");
+          console.log("\r\nSorry we don't have enough stock\r\n");
           buyPrompt();
         }
         else {
-          console.log("Your order was made successfully ");
+          console.log("\r\n~~~~~ Your order was made successfully! ~~~~~\r\n");
           var totalCost = res[i].PRICE * inputs.Quantity;
-          console.log("Your total cost for " + res[i].PRODUCT_NAME + " is " + totalCost);
-          console.log("Thank you for shopping in Bamazon. Have a good day!");
+          console.log("\r\nYour total cost for " + res[i].PRODUCT_NAME + " is " + totalCost);
+          console.log("\r\n+++++ Thank you for shopping on Bamazon. Have a good day! +++++\r\n");
           var updateQuantity = res[i].QUANTITY - inputs.Quantity;
-          console.log("Quantity of " + res[i].PRODUCT_NAME + " has been updated");
+          console.log("\r\nQuantity of " + res[i].PRODUCT_NAME + " has been updated to " + updateQuantity);
           connection.query("UPDATE products SET QUANTITY = " + updateQuantity + " WHERE ID = " + res[i].ID, function(err, res){
           checkOut();
           });
@@ -84,16 +84,16 @@ function checkOut(){
   inquirer.prompt([
     {
       name: "buyMore",
-      type: "checkbox",
-      message: "Do you want to buy more products?",
+      type: "list",
+      message: "\r\nDo you want to buy more products?\r\n",
       choices:["Yes", "NO"]
     }
   ]).then (function(res) {
     if (res.Yes == true) {
-      console.log("Let's shop more");
+      console.log("\r\nLet's shop more!\r\n");
       buyPrompt();
     } else {
-      console.log("Thank you for your business with Bamazon");
+      console.log("\r\n>>>>> Thank you for your business with Bamazon <<<<<\r\n");
       connection.end();
     }
   })
